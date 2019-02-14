4292df9be350aed57be534118e6f80e7c43828bc
Pieter Koornhof <koornhof.pieter@gmail.com>
2019-02-14 16:58:15 +0200

    Chore: Add tax config

--- src/tfsa/config.cljs
+++ src/tfsa/config.cljs
@@ -1,5 +1,14 @@
-(ns tfsa.config)
+(ns tfsa.config
+  (:require [cljs-time.core :as time]))
 
 (def first-tfsa-year 2015)
 
 (def lifetime-limit 500000)
+
+(def tax-year-limits
+  (merge
+   {2015 30000
+    2016 30000}
+   (->> (range 2017 (inc (time/year (time/now))))
+        (map #(hash-map % 33000))
+        (into {}))))
